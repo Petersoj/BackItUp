83b18edde1c1f300d3d2202d189c213b433aca30
Jacob Peterson <petersojj@gmail.com>
2017-08-30 16:39:10 -0600

    Initial project with docs

---
+++ BackItUp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BackItUp
+//
+//  Created by Jacob Peterson on 8/30/17.
+//  Copyright © 2017 Jacob Peterson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BackItUp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BackItUp
+//
+//  Created by Jacob Peterson on 8/30/17.
+//  Copyright © 2017 Jacob Peterson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
